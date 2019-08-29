a418ae88ce474709563125b17355366ccc5e3314
Jagaa <jagaa1103@gmail.com>
2019-08-29 17:10:37 +0900

    init project

---
+++ GymTrack/GymTrack/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  GymTrack
+//
+//  Created by Enkhjargal Gansukh on 29/08/2019.
+//  Copyright © 2019 Enkhjargal Gansukh. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
